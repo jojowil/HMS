34ec991e361fba8d1114937f929392d74fd692bb
jojowil <jojowil@gmail.com>
2025-07-07 05:26:55 -0400

    more code...

--- hms.py
+++ hms.py
@@ -14,6 +14,15 @@ def usage(msg=None):
 def process():
     print('Processing')
 
+def do_freelist(cur):
+    print('Free list...')
+    # Execute a query
+    cur.execute('SELECT ip from hms_ip where host is null')
+    for row in cur:
+        # Access data by index (e.g., row[0], row[1])
+        print(f'FREE: {row[0]}')
+    print('\nTotal free IPs is', cur.rowcount)
+
 def main():
     #
     # Get password for user with table access
@@ -78,18 +87,47 @@ def main():
     if len(mode) > 1:
         usage('Choose one of add, modify, delete, list, or free.')
 
+    # Connect to server
+    try:
+        cnx = mysql.connector.connect(
+        host='127.0.0.1',
+        port=3306,
+        user='hms',
+        database='hms',
+        password=dbpwd)
+    except mysql.connector.Error as err:
+        print(f'Error connecting to MySQL: {err}')
+        sys.exit(2)
+
+    # Get a cursor
+    cur = cnx.cursor()
+
     if mode == 'A':
         print('Adding...')
+        if host is None:
+            print('No host specified')
+        if ip is None:
+            print('No IP specified. Using next available.')
+            cur.execute('SELECT ip FROM ips WHERE host is null limit 1')
+            if (cur.rowcount == 0):
+                print('No free IPs available.')
+                sys.exit(3)
+            ip = cur.fetchone()
     elif mode == 'M':
         print('Modifying...')
+        if host is None or ip is None:
+            print('Must specifiy ')
     elif mode == 'D':
         print('Deleting...')
-    elif mode == 'G':
+    elif mode == 'L':
         print('Getting...')
     elif mode == 'F':
-        print('Free list...')
+        do_freelist(cur)
     else:
         usage('FATAL: Unknown mode')
+
+    # Close connection
+    cnx.close()
 
 if __name__ == '__main__':
     main()
