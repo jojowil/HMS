5a3dceb3f15fd4d24642e35403c441f7bdbd5915
jojowil <jojowil@gmail.com>
2025-07-07 08:58:22 -0400

    completed add (mostly), added check functions and updated notes for unique qualifiers.

--- hms.py
+++ hms.py
@@ -11,11 +11,96 @@ def usage(msg=None):
     print("\n\t-A add\n\t-M modify\n\t-D delete\n\t-L list\n\t-F free list\n")
     sys.exit(1)
 
-def process():
-    print('Processing')
+def check_host_inuse(cnx, host):
+    cur = cnx.cursor()
+    cur.execute("SELECT COUNT(*) FROM hms_ip WHERE host = %s ", (host,))
+    inuse = True if cur.rowcount > 0 else False
+    # must fetch or unread result
+    cur.fetchall()
+    return inuse
 
-def do_freelist(cur):
+def check_ip_inuse(cnx, ip):
+    cur = cnx.cursor()
+    cur.execute("SELECT COUNT(*) FROM hms_ip WHERE ip = %s and host is not null", (ip,))
+    inuse = True if cur.rowcount > 0 else False
+    # must fetch or unread result
+    cur.fetchall()
+    return inuse
+
+
+def do_add(cnx, ip, host, desc, mac, dhcp):
+    print('Adding...')
+    cur = cnx.cursor()
+    # mac is optional
+    if mac is None and dhcp == 'Y':
+        print('Cannot use DHCP without a mac!')
+        sys.exit(3)
+    # host is required and unique
+    if host is None:
+        print('No host name specified.')
+        sys.exit(3)
+    elif check_host_inuse(cnx, host):
+            print('Host %s is already in use.' % host)
+            sys.exit(3)
+    # desc is optional, but recommended.
+    if desc is None:
+        print('No description specified. You should really describe the purpose.')
+    # if you cannot afford and IP, one will be provided for you.
+    if ip is None:
+        print('No IP specified. Using next available.')
+        cur.execute('SELECT ip FROM hms_ip WHERE host is null limit 1')
+        if cur.rowcount == 0:
+            print('No free IPs available.')
+            sys.exit(3)
+        ip = cur.fetchone()
+    else:
+        if check_ip_inuse(cnx, ip):
+            print('IP already in use.')
+            sys.exit(3)
+    query = "update hms_ip set host='%s'" % (host)
+    if mac is not None:
+        query += ",mac='%s'" % (mac)
+    if desc is not None:
+        query += ",descr='%s'" % (desc)
+    if dhcp is not None:
+        query += ",dhcp='%s'" % (dhcp)
+    query += " where ip='%s';" % (ip)
+
+    print(query)
+    cur.execute(query)
+    cnx.commit()
+
+    # Check the number of affected rows
+    affected_rows = cur.rowcount
+
+    if affected_rows > 0:
+        print(f"{affected_rows} record(s) updated successfully.")
+    else:
+        print("No records were updated (either no matching rows or data was already the same).")
+
+def do_modify(cnx, ip, desc, mac, dhcp):
+    return
+
+def do_delete(cnx, ip, desc, mac):
+    print('Deleting...')
+    return
+
+def do_list(cnx, ip, host):
+    if (ip is None and host is None) or (ip is not None and host is None):
+        print('Must specify either ip or host.')
+        sys.exit(4)
+    query = 'select * from hms_ip where '
+    if ip is not None:
+        query += "ip = '%s'" % (ip)
+    else:
+        query += "host = '%s'" % (host)
+    cur = cnx.cursor()
+    cur.execute(query)
+    return
+
+def do_freelist(cnx):
     print('Free list...')
+    cur = cnx.cursor()
     # Execute a query
     cur.execute('SELECT ip from hms_ip where host is null')
     for row in cur:
@@ -99,30 +184,18 @@ def main():
         print(f'Error connecting to MySQL: {err}')
         sys.exit(2)
 
-    # Get a cursor
-    cur = cnx.cursor()
-
     if mode == 'A':
-        print('Adding...')
-        if host is None:
-            print('No host specified')
-        if ip is None:
-            print('No IP specified. Using next available.')
-            cur.execute('SELECT ip FROM ips WHERE host is null limit 1')
-            if (cur.rowcount == 0):
-                print('No free IPs available.')
-                sys.exit(3)
-            ip = cur.fetchone()
+        do_add(cnx, ip, host, desc, mac, dhcp)
     elif mode == 'M':
         print('Modifying...')
         if host is None or ip is None:
             print('Must specifiy ')
     elif mode == 'D':
-        print('Deleting...')
+        do_delete(cnx, host)
     elif mode == 'L':
         print('Getting...')
     elif mode == 'F':
-        do_freelist(cur)
+        do_freelist(cnx)
     else:
         usage('FATAL: Unknown mode')
 
